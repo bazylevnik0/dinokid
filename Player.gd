5f0be356556c54b39870a827a2242f0fb549ed0d
bazylevnik0 <bazylevnik0@gmail.com>
2024-08-11 10:09:05 +0300

    basic simplified control

--- Player.gd
+++ Player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
-@export var speed = 0.1
-# The downward acceleration when in the air, in meters per second squared.
+@export var max_speed_forward  = 10
+@export var max_speed_backward = -1
 @export var fall_acceleration = 1
+@export var max_jump_height = 100
 
 var target_velocity = Vector3.ZERO
+var target_jump_height = 0
+var target_speed = 0
 
 
 func _physics_process(delta):
@@ -15,19 +17,38 @@ func _physics_process(delta):
 		rotation.y -= PI/2
 	if Input.is_action_just_released("move_left"):
 		rotation.y += PI/2
+	
 	if Input.is_action_pressed("move_backward"):
+		target_speed -= 0.1;
+		if target_speed <= max_speed_backward :
+			target_speed = max_speed_backward;
 		direction.x += 1
+	if Input.is_action_just_released("move_backward"):
+		target_speed = 0
+	
 	if Input.is_action_pressed("move_forward"):
+		target_speed += 0.1;
+		if target_speed >= max_speed_forward :
+			target_speed = max_speed_forward
 		direction.x -= 1
-
+	if Input.is_action_just_released("move_forward"):
+		target_speed = 0
+	
+	
+	if Input.is_action_pressed("jump"):
+		target_jump_height+=1	
+		if target_jump_height >= max_jump_height :
+			target_jump_height = max_jump_height
+	if Input.is_action_just_released("jump"):
+		position.y += target_jump_height
+		target_jump_height = 0
+		
 	if direction != Vector3.ZERO:
-		direction = direction.normalized()
 		direction = direction.rotated(Vector3.UP, rotation.y)
-		#$Model.basis = Basis.looking_at(direction)
 
 	# Ground Velocity	
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	target_velocity.x = direction.x * abs(target_speed)
+	target_velocity.z = direction.z * abs(target_speed)
 
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
