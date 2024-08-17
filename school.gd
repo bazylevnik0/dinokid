12a00e46d4c58dd539ff6d2de4f82c19d39b9180
bazylevnik0 <bazylevnik0@gmail.com>
2024-08-17 09:38:45 +0300

    fix rotate in school

--- school.gd
+++ school.gd
@@ -9,4 +9,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$StaticBody3D/school/AnimationPlayer.play("Animation")
+	$StaticBody3D/CollisionRotate.rotate_y(0.01)
 	pass
