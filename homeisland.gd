798321201de2fc5bb98466edb74e4a57cab6de55
bazylevnik0 <bazylevnik0@gmail.com>
2024-08-25 10:16:22 +0300

    working on the mountain inside

---
+++ homeisland.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_mountain_inside_area_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Text.text = "Dino, please go outside and find me..."
+		$Text.visible = true
+	pass # Replace with function body.
+	
+
+
+func _on_mountain_inside_area_body_exited(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Text.text = "..."
+		$Text.visible = false
+	pass # Replace with function body.
