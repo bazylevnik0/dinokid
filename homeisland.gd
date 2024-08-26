55b6e2a52753eb5191ad13ced2b553705430e8a4
bazylevnik0 <bazylevnik0@gmail.com>
2024-08-26 17:53:11 +0300

    add collision boxes for knifes and river

--- homeisland.gd
+++ homeisland.gd
@@ -1,16 +1,12 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
 
 func _on_mountain_inside_area_body_entered(body: Node3D) -> void:
 	if body.name == "Player" :
@@ -19,9 +15,20 @@ func _on_mountain_inside_area_body_entered(body: Node3D) -> void:
 	pass # Replace with function body.
 	
 
-
 func _on_mountain_inside_area_body_exited(body: Node3D) -> void:
 	if body.name == "Player" :
 		$Text.text = "..."
 		$Text.visible = false
+	pass # Replace with function body.
+	
+	
+func _on_mountain_down_area_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Dino/Player.position = Vector3(-0.053, 1.108, 0.386)
+	pass # Replace with function body.
+
+
+func _on_river_water_area_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Dino/Player.position = Vector3(-0.053, 1.108, 0.386)
 	pass # Replace with function body.
