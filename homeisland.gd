6ab8fbcf7cdd8c360f0962f0e04644d0a42f8499
bazylevnik0 <bazylevnik0@gmail.com>
2024-08-30 22:59:29 +0300

    homeisland: start working on forest

--- homeisland.gd
+++ homeisland.gd
@@ -33,3 +33,17 @@ func _on_river_water_area_body_entered(body: Node3D) -> void:
 	if body.name == "Player" :
 		$Dino/Player.position = Vector3(-0.053, 1.108, 0.386)
 	pass # Replace with function body.
+
+
+func _on_beach_place_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Text.text = "Dino: So sunny day to play on the beach but I must to find my mom..."
+		$Text.visible = true
+	pass # Replace with function body.
+
+
+func _on_beach_place_body_exited(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Text.text = "..."
+		$Text.visible = false
+	pass # Replace with function body.
