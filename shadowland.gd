530323f5426bd75974061c615a5f49cadce2be67
bazylevnik0 <bazylevnik0@gmail.com>
2024-09-05 19:01:50 +0300

    shadowland: add danger doors

---
+++ shadowland.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_danger_door_1_area_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Dino/Player.position = Vector3(-0.055, 1.257, 0.184)
+	pass # Replace with function body.
+
+
+func _on_danger_door_2_area_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Dino/Player.position = Vector3(-0.055, 1.257, 0.184)
+	pass # Replace with function body.
+
+
+func _on_danger_door_3_area_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		$Dino/Player.position = Vector3(-0.055, 1.257, 0.184)
+	pass # Replace with function body.
