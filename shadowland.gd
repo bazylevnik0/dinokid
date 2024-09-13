b932583fdb009b4771aabf5e741771129a2dffb6
bazylevnik0 <bazylevnik0@gmail.com>
2024-09-13 22:27:37 +0300

    shadowland: add outro

--- shadowland.gd
+++ shadowland.gd
@@ -84,3 +84,12 @@ func _on_sun_zone_body_entered(body: Node3D) -> void:
 	if body.name == "Player" :
 		$Dino/Player.position = Vector3(-0.055, 1.257, 0.184)
 	pass # Replace with function body.
+
+
+
+
+func _on_monument_exit_body_entered(body: Node3D) -> void:
+	if body.name == "Player" :
+		print("hehe")
+		get_tree().change_scene_to_file('res://dino_shadowland.tscn')
+	pass # Replace with function body.
